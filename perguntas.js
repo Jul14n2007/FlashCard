769533a3063ab780859f56dc6c2fb6ea0809e0ae
Jhulian <125598309+Jul14n2007@users.noreply.github.com>
2024-11-29 15:36:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ciencias',
+    'Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
+    'Entre 4 a 6 litros. São retirados 450 mililitros'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos estados tem o Brasil?',
+    'O Brasil tem 26 estados e 1 distrito federal'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz " Tudo bem com você? " em Inglês?',
+    'Oi em ingles é " How are you? "'
+)
